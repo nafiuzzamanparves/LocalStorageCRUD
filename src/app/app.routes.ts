039d6a772722e6b22168a13b8da69303ea6983cd
Your Name <you@example.com>
2025-02-18 10:44:18 +0600

    added two pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { CreateUserComponent } from './create-user/create-user.component';
+import { ListUserComponent } from './list-user/list-user.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    // { path: '', component: HomeComponent },
+    { path: 'create', component: CreateUserComponent },
+    { path: 'list', component: ListUserComponent },
+];
