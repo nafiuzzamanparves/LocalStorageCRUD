86d304ddbb69fa5c435ead804f41b3ec0450da12
Your Name <you@example.com>
2025-02-18 11:38:46 +0600

    create user

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HomeComponent } from "./home/home.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HomeComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'LocalStorageCRUD';
+
+
+}
+
+export class User {
+  name: string;
+  age: number;
+  phone: string;
+  address: string;
+
+  constructor(name: string, age: number, phone: string, address: string) {
+    this.name = name;
+    this.age = age;
+    this.phone = phone;
+    this.address = address;
+  }
 }
--- src/app/create-user/create-user.component.ts
+++ src/app/create-user/create-user.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { User } from '../app.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-create-user',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './create-user.component.html',
-  styleUrl: './create-user.component.css'
+  styleUrl: './create-user.component.css',
+  standalone: true
 })
-export class CreateUserComponent {
+export class CreateUserComponent implements OnInit {
+  user: User = new User('', 0, '', '');
+
+  ngOnInit(): void {
+    console.log('CreateUserComponent');
+
+    // write a text to local storage
+    localStorage.setItem('name', 'John');
+    localStorage.setItem('age', '25');
+    localStorage.setItem('phone', '1234567890');
+    localStorage.setItem('address', '123 Main Street');
+  }
+
+  onSubmit() {
+    let users = JSON.parse(localStorage.getItem('users') || '[]');
+    // debugger;
+    users.push(this.user);
+
+    localStorage.setItem('users', JSON.stringify(users));
+
+    this.user = new User('', 0, '', '');
+    alert('User added successfully!');
+  }
 
 }
